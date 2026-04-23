3998ac2b529c213393921019765ce0053ee1c007
LJ <lilian.janicot@centrale-med.fr>
2026-04-23 15:53:37 +0200

    🚚 🎨 All 🐛  🎨 Footer
    - All : ajout d'emoji sur les noms des fichiers
    - Footer : correction du bug du scroll en bas de la page sur le footer + footer plus beau

--- components/components.py
+++ components/components.py
@@ -2,18 +2,6 @@ import streamlit as st
 from streamlit_extras.bottom_container import *
 from pathlib import Path
 
-
-def footer():
-    with bottom() :
-        st.markdown("---")
-        st.markdown(
-        """
-        <font size="2">*Ce site n'a fait l'objet d'aucune validation officielle de la part de l'école. Il est destiné à être un outil de communication et de partage d'informations pour les étudiants.
-        Les informations sur le site peuvent donc être incomplètes ou obsolètes, et il est recommandé de vérifier les informations auprès des sources officielles de l'école pour toute question ou préoccupation.*</font>
-        """, unsafe_allow_html=True
-        )
-        st.write("© 2026 Lilian Janicot. Le site n'a pas été généré par une IA. Version 0.1")
-
 def course_card(course_name: str, course_desc: str, course_teachers: str=None, prerequisites: str = None):
     st.subheader(course_name)
     if course_teachers:
@@ -26,3 +14,22 @@ def path_logo():
     BASE_DIR = Path(__file__).resolve().parent.parent
     image = BASE_DIR / "components" / "images" / "Do_it.png"
     return image
+
+def footer():
+    # Footer
+    footer = """
+    <style>
+    .footer {
+        width: 100%;
+        color: gray;
+        text-align: center;
+        padding: 20px 0;
+        font-size: 12px;
+    }
+    </style>
+    <div class="footer">
+    Ce site n'a fait l'objet d'aucune validation officielle de la part de l'école. Il est destiné à être un outil de communication et de partage d'informations pour les étudiants. Les informations sur le site peuvent donc être incomplètes ou obsolètes, et il est recommandé de vérifier les informations auprès des sources officielles de l'école pour toute question ou préoccupation.
+    <br>© 2026 Lilian Janicot. Ce site n'a pas été généré par une IA. Version 0.1
+    </div>
+    """
+    st.markdown(footer, unsafe_allow_html=True)
